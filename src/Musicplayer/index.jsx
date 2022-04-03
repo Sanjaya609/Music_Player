3197e8885c892de123ae1c7c562f1530c5fa1f63
Sanjaya609 <Sanjayakhatri5@gmail.com>
2022-04-03 20:02:01 +0545

    feat: Homepage modified

--- src/Musicplayer/index.jsx
+++ src/Musicplayer/index.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { Route, Router, Routes } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { NavBar } from '../Components/NavBar';
 import { Player } from '../Components/Player';
 import { Bodymain } from '../Pages/Bodymain';
@@ -8,11 +8,18 @@ import Music from '../Pages/Music';
 import Loader from '../Components/Loader';
 import { Explore } from '../Components/Explore';
 import { useNavigate } from 'react-router';
+import { fetchGenre } from '../Redux/Action/fetchGenre';
 
 const Musicplayer = () => {
+    const dispatch= useDispatch();
     const songList = useSelector((state) => state.music.musics);
     const isLoading = useSelector((state) => state.music.isLoading);
     const navigate = useNavigate();
+    useEffect(() => {
+        const genre='country';
+        dispatch(fetchGenre(genre));
+    }, []);
+    
     return (
         <>
             <div className="header shadow bg-white rounded sticky">
@@ -21,7 +28,7 @@ const Musicplayer = () => {
             <div className="container">
                 <div className="row body-container" style={{ 'justifyContent': 'center', height: "480px" }}>
                     <Routes>
-                        <Route path='' element={<Bodymain />}></Route>
+                        <Route path='' element={<Explore />}></Route>
                         <Route path='/explore/*' element={<Explore />} >
                             
                         </Route>
